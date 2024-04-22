f149c36dfbc25358941f570f4199de14ba3fe447
Gab Dev <suppershock777@gmail.com>
2024-04-22 17:19:20 -0300

    Feat: iniciado aulas node

--- aula02/app.js
+++ aula02/app.js
@@ -1,3 +1,10 @@
-const fs = require('fs');
+const fs = require("fs");
 
-fs.writeFileSync('mensagem.txt', 'Essa coisa');
+fs.writeFileSync(
+   "mensagem.txt",
+   "Essa é a nossa segunda mensagem escrita com o módulo fs!"
+);
+
+
+const fs2 = require("fs");
+fs2.appendFileSync('mensagem.txt', '\nIsto é uma atividade de NODE', 'utf8');
